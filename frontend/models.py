fc4a3180b99e0f8415954beea9124373f1c8de76
Esther <estherwangari206@gmail.com>
2022-08-31 10:52:40 -0300

    created task model

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from   django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    due_date = models.DateField()
+    HIGH = "high"
+    MEDIUM = "medium"
+    LOW = "low"
+
+    TASK_PRIORITIES=(
+        (LOW , "Low"),
+        (MEDIUM , "Medium"),
+        (HIGH , "High")
+
+    )
+    priority = models.CharField( max_length=20 , choices = TASK_PRIORITIES , default=LOW)
+    complete = models.BooleanField(default= False)
+    user = models.ForeignKey(User , on_delete=models.CASCADE, null=True, blank=True)
+    ##Timestamps: They  record when a db was created and when it was last updated
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
